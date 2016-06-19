4447197b8d730d16fbca88f7516ee0218a51eb5b
Superjom <yanchunwei@outlook.com>
2016-06-19 15:19:25 +0800

    add rsync_clone.py

---
+++ bin/rsync_clone.py
@@ -0,0 +1,12 @@
+import sys
+import logging
+from RsyncContext import RsyncManager, RsyncContext, set_debug
+
+if len(sys.argv) != 3:
+    logging.error('missing arguments')
+    logging.error('rsync_clone.py [remote_path] [local_path]')
+    sys.exit(-1)
+
+rsync_manager = RsyncManager()
+
+rsync_manager.clone(*sys.argv[1:])
